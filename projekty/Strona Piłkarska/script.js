5cc4cb0a380a70a7c7e89b3e615127136a664496
rafi9898 <r-podraza@wp.pl>
2017-05-02 15:40:18 +0200

    Dodatki [Wersja: 1.0.2]

--- projekty/Strona Piłkarska/script.js
+++ projekty/Strona Piłkarska/script.js
@@ -4,3 +4,14 @@ $('.pick').slideToggle();
 })
 });
 
+$(document).ready(function(){
+ 
+		$('*[data-animate]').addClass('hide').each(function(){
+      $(this).viewportChecker({
+        classToAdd: 'show animated ' + $(this).data('animate'),
+        classToRemove: 'hide',
+        offset: '30%'
+      });
+    });
+ 
+	});
